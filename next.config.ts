210426948a025c99587662042b82f084ac5d9342
Ami Hollander <amhollan@microsoft.com>
2026-03-06 18:34:50 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  ...(isGithubPages && {
+    basePath: '/mini-games',
+  }),
 };
 
 export default withNextIntl(nextConfig);
